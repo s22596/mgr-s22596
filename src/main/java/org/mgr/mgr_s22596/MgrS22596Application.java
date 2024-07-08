8e3348bc956572d9b3c3676190168a110692448c
s22596 <s22596@pjwstk.edu.pl>
2024-07-08 21:32:25 +0200

    init

---
+++ src/main/java/org/mgr/mgr_s22596/MgrS22596Application.java
@@ -0,0 +1,13 @@
+package org.mgr.mgr_s22596;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrS22596Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgrS22596Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/mgr/mgr_s22596/MgrS22596ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mgr.mgr_s22596;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgrS22596ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
